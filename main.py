4df83eb6e10d322e3569af61c34f990e687477fe
pomponchik <zheni-b@yandex.ru>
2022-05-16 20:42:46 +0300

    drafting of the locks and some text in the readme

--- main.py
+++ main.py
@@ -1,25 +1,38 @@
-from threading import Thread, RLock, get_ident
+from threading import Thread, RLock, get_ident, get_native_id
 import threading
 
+class LockIntersectionError(ValueError):
+    pass
 
 class SequenceAgent:
     def __init__(self, id, parent, sequence):
+        self.lock = RLock()
         self.id = id
 
         self.parent = parent
 
         self.width = sequence.width
-        self.current_value = parent.get_new_start()
+
+        self.current_value = parent.get_new_start(self.width)
         self.max_value = self.current_value + self.width
 
     def get_value(self):
-        if self.current_value > self.max_value:
-            self.current_value = parent.get_new_start()
-            self.max_value = self.current_value + self.width
-        return self.current_value
+        print('pre 1')
+        with self.lock:
+            if self.current_value > self.max_value:
+                while True:
+                    try:
+                        self.current_value = self.parent.get_new_start(self.width)
+                        break
+                    except LockIntersectionError as e:
+                        pass
+                self.max_value = self.current_value + self.width
+            result = self.current_value
+            self.current_value += 1
+            return result
 
 class MiddleSequence:
-    def __init__(self, sequence, parent, width, height):
+    def __init__(self, sequence, parent, width, height, is_root):
         if width < 2:
             raise ValueError('The branchiness of the tree cannot be less than 2.')
 
@@ -32,15 +45,24 @@ class MiddleSequence:
         self.width = width
         self.height = height
 
-        self.is_root = False
+        self.is_root = is_root
+
+        if not self.is_root:
+            self.current_value = parent.get_new_start(self.width)
+        else:
+            self.current_value = self.sequence.start
+        self.max_value = self.current_value + self.width
 
     def set_root(self, flag):
-        self.is_root = flag
+        print('pre 2')
+        with self.lock:
+            self.is_root = flag
 
     def get_new_middle(self, width):
+        print('pre 3')
         with self.lock:
             if len(self.childs) < self.width:
-                new_middle = MiddleSequence(self.sequence, self, width, self.height - 1)
+                new_middle = MiddleSequence(self.sequence, self, width, self.height - 1, False)
                 self.register_child(new_middle, None)
                 return new_middle
             else:
@@ -48,6 +70,7 @@ class MiddleSequence:
                 return brother.get_new_middle(width)
 
     def register_child(self, child, id):
+        print('pre 4')
         with self.lock:
             if len(self.childs) < self.width:
                 self.childs.append(child)
@@ -56,54 +79,80 @@ class MiddleSequence:
                 new_middle.register_child(child, id)
 
     def create_agent(self, id):
+        print('pre 5')
         with self.lock:
             if self.height == 1:
                 agent = SequenceAgent(id, self, self.sequence)
                 self.register_child(agent, id)
                 return agent
             else:
-                self.childs[-1].create_agent(id)
+                return self.childs[-1].create_agent(id)
 
     def register_parent(self, parent):
+        print('pre 6')
         with self.lock:
             self.parent = parent
 
-    def get_new_start(self):
+    def get_new_start(self, width):
+        print('pre 7')
+        locked = self.lock.acquire(blocking=False)
+        if not locked:
+            raise LockIntersectionError('Locks are intersected.')
         with self.lock:
-            pass
-
+            if self.is_root:
+                result = self.current_value
+                self.current_value += width
+                self.max_value = self.current_value
+            else:
+                if self.current_value > self.max_value:
+                    print(self.is_root)
+                    self.current_value = self.parent.get_new_start(self.width)
+                    self.max_value = self.current_value + self.width
+                result = self.current_value
+                self.current_value += width
+            return result
 
 class Sequence:
     def __init__(self, width, start=0):
         self.lock = RLock()
 
-        self.root = MiddleSequence(self, self, width, 1)
+        self.width = width
+
+        self.start = start
+
+        self.root = MiddleSequence(self, self, width, 1, True)
         self.root.set_root(True)
         self.leaves = {}
 
-        self.width = width
-
     def create_agent(self, id):
-        self.root.create_agent(id)
+        print('pre 8')
+        with self.lock:
+            return self.root.create_agent(id)
 
     def register_child(self, child, id):
-        self.leaves[id] = child
+        print('pre 9')
+        with self.lock:
+            self.leaves[id] = child
 
     def get_agent(self, id):
+        print('pre 10')
         with self.lock:
+            print('post 10')
             if id in self.leaves:
-                return leaves[id]
+                return self.leaves[id]
             agent = self.create_agent(id)
             self.register_child(agent, id)
             return agent
 
-    def get_new_middle(self):
+    def get_new_middle(self, width):
+        print('pre 11')
         with self.lock:
-            new_root = MiddleSeries(self, self, self.root.width * self.width, self.root.height + 1)
+            new_root = MiddleSequence(self, self, self.root.width * self.width, self.root.height + 1, True)
+            new_root.current_value = self.root.current_value
             new_root.register_child(self.root, None)
 
             self.root.set_root(False)
-            new_root.set_root(True)
+            self.root.parent = new_root
 
             self.root = new_root
 
@@ -111,19 +160,44 @@ class Sequence:
 
 seq = Sequence(10)
 data = set()
+data2 = []
 number_of_threads = 1000
 numbers_per_thread = 10000
+rs = set()
+kek = set()
 
 def func():
-    agent = seq.get_agent(get_ident())
+    kek.add(get_native_id())
+    agent = seq.get_agent(get_native_id())
     for _ in range(numbers_per_thread):
-        data.add(agent.get_value())
+        value = agent.get_value()
+        data.add(value)
+        print(value)
+        #data2.append(value)
+        #rs.add(agent.parent.sequence.root.is_root)
+    #"""
 
 threads = [Thread(target=func, args=()) for _ in range(number_of_threads)]
+
 
 for thread in threads:
     thread.start()
 for thread in threads:
-    thread.loin()
+    thread.join()
 
 print(len(data))
+print(len(data2))
+print(rs)
+print(len(seq.leaves))
+print('len threads:', len(threads))
+
+"""
+k = set()
+r = []
+for n in data2:
+    if n in k:
+        r.append(n)
+    k.add(n)
+
+"""
+#print(r)
