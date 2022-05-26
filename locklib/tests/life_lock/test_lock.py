edc9354d888f7d79255a8545a7049dc425e96951
pomponchik <zheni-b@yandex.ru>
2022-05-26 21:00:14 +0300

    +1 test (it's not working)

--- locklib/tests/life_lock/test_lock.py
+++ locklib/tests/life_lock/test_lock.py
@@ -1,5 +1,5 @@
 from queue import Queue
-from threading import Thread
+from threading import Thread, Lock
 
 import pytest
 
@@ -85,3 +85,95 @@ def test_raise_when_simple_deadlock():
         thread_2.join()
 
         assert queue.get()
+
+
+@pytest.mark.timeout(5)
+def test_raise_when_not_so_simple_deadlock():
+    number_of_attempts = 50
+
+    lock_1 = LifeLock()
+    lock_2 = LifeLock()
+    lock_3 = LifeLock()
+
+    queue = Queue()
+
+    for _ in range(number_of_attempts):
+        flag = False
+        cycles = 0
+        lock = Lock()
+        def function_1():
+            nonlocal flag
+            try:
+                while True:
+                    with lock_1:
+                        with lock_2:
+                            with lock_3:
+                                print(1)
+                                if flag:
+                                    print(11)
+                                    break
+            except DeadLockError as e:
+                with lock:
+                    cycles += 1
+                    if cycles == 2:
+                        flag = True
+                queue.put(True)
+                raise e
+
+        def function_2():
+            nonlocal flag
+            try:
+                while True:
+                    with lock_2:
+                        with lock_3:
+                            with lock_1:
+                                print(2)
+                                if flag:
+                                    print(21)
+                                    break
+            except DeadLockError as e:
+                with lock:
+                    cycles += 1
+                    if cycles == 2:
+                        flag = True
+                queue.put(True)
+                raise e
+
+        def function_3():
+            nonlocal flag
+            try:
+                while True:
+                    with lock_3:
+                        with lock_1:
+                            with lock_2:
+                                print(3)
+                                if flag:
+                                    print(31)
+                                    break
+            except DeadLockError as e:
+                with lock:
+                    cycles += 1
+                    if cycles == 2:
+                        flag = True
+                queue.put(True)
+                raise e
+
+
+        thread_1 = Thread(target=function_1)
+        thread_2 = Thread(target=function_2)
+        thread_3 = Thread(target=function_3)
+        thread_1.start()
+        thread_2.start()
+        thread_3.start()
+
+        thread_1.join()
+        thread_2.join()
+        thread_3.join()
+
+        counter = 0
+
+        for _ in range(2):
+            queue.get()
+            counter += 1
+
+        assert counter == 2
