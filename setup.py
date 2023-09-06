d6486de72f03ff4dc9b36eb9ed28455949d66cc9
Evgeniy Blinov <zheni-b@yandex.ru>
2023-09-06 12:34:54 +0300

    classifiers

--- setup.py
+++ setup.py
@@ -17,7 +17,10 @@ setup(
     packages=find_packages(),
     install_requires=requirements,
     classifiers=[
+        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "License :: OSI Approved :: MIT License",
     ],
 )
