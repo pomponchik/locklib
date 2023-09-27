d89d44d738a15fdca057740c0dc4e21afb644462
Evgeniy Blinov <zheni-b@yandex.ru>
2023-09-28 02:17:56 +0300

    a new version tag

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ requirements = []
 
 setup(
     name='locklib',
-    version='0.0.7',
+    version='0.0.8',
     author='Evgeniy Blinov',
     author_email='zheni-b@yandex.ru',
     description='A wonderful life without deadlocks',
