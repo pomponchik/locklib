f5c888b2fd34fc42e997fcdd49964f5a2767ade2
Evgeniy Blinov <zheni-b@yandex.ru>
2024-02-26 15:07:47 +0300

    the ContextLockProtocol now is inherited from the LockProtocol

--- locklib/protocols/context_lock.py
+++ locklib/protocols/context_lock.py
@@ -6,17 +6,13 @@ try:
 except ImportError:  # pragma: no cover
     from typing_extensions import Protocol, runtime_checkable  # type: ignore[assignment]
 
+from locklib.protocols.lock import LockProtocol
+
 
 @runtime_checkable
-class ContextLockProtocol(Protocol):
+class ContextLockProtocol(LockProtocol, Protocol):
     def __enter__(self) -> Any:
         raise NotImplementedError('Do not use the protocol as a lock.')
 
     def __exit__(self, exception_type: Optional[Type[BaseException]], exception_value: Optional[BaseException], traceback: Optional[TracebackType]) -> bool:
-        raise NotImplementedError('Do not use the protocol as a lock.')
-
-    def acquire(self) -> None:
-        raise NotImplementedError('Do not use the protocol as a lock.')
-
-    def release(self) -> None:
         raise NotImplementedError('Do not use the protocol as a lock.')
