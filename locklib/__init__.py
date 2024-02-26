124ec897d45089cd3f28a72249b79694ebe0b3a6
Evgeniy Blinov <zheni-b@yandex.ru>
2024-02-26 15:15:21 +0300

    short imports

--- locklib/__init__.py
+++ locklib/__init__.py
@@ -1,2 +1,4 @@
 from locklib.locks.smart_lock.lock import SmartLock as SmartLock  # noqa: F401
 from locklib.errors import DeadLockError as DeadLockError  # noqa: F401
+from locklib.protocols.lock import LockProtocol as LockProtocol  # noqa: F401
+from locklib.protocols.context_lock import ContextLockProtocol as ContextLockProtocol  # noqa: F401
